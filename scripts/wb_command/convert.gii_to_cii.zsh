aac6aef906d9d23772d3f8a358a8f840b5e32c49
psb629 <psb629@gmail.com>
2025-09-17 18:42:36 -0400

    what is the metric?

--- scripts/wb_command/convert.gii_to_cii.zsh
+++ scripts/wb_command/convert.gii_to_cii.zsh
@@ -2,6 +2,7 @@
 
 ##############################################################
 gs='n'
+type_='dscalar'
 ## $# = the number of arguments
 while (( $# )); do
 	key="$1"
@@ -9,30 +10,22 @@ while (( $# )); do
 ##		pattern)
 ##			sentence
 ##		;;
+		-i | --input)
+			input="$2"
+		;;
 		-s | --subject)
 			subj="$2"
 		;;
-		-h | --hem)
-			hem="$2"
+		-t | --type)
+			type_="$2"
 		;;
-		-gs | --glmsingle)
-			gs="$2"
-		;;
-		-g | --glm)
-			glm="$2"
-		;;
-		-m | --map)
-			map="$2"
-		;;
-		--help)
+		-h | --help)
 			cat <<EOF
 ===========================================
 Options:
+	-i | --input
 	-s | --subject
-	-h | --hem
-	-g | --glm
-	-m | --map
-	-gs | --glmsingle (default: n)
+	-t | --type
 ===========================================
 EOF
 		exit
@@ -40,42 +33,38 @@ EOF
 	esac
 	shift ##takes one argument
 done
-glm1d=`printf "%1d\n" $glm`
 hem=${hem:u}
 ##############################################################
-dir_root='/Volumes/Diedrichsen_data$/data/SeqSpatialSupp_fMRI'
+fname=${input:t}
+dname=${input:h}
+##############################################################
+case "$(uname)" in
+	Darwin)
+		dir_root='/Volumes/Diedrichsen_data$/data/SeqSpatialSupp_fMRI'
+	;;
+	Linux)
+		dir_root='/mnt/f/SeqSpatialSupp_fMRI'
+	;;
+esac
+dir_fs=$HOME/github/fs_LR_32
 dir_surf=$dir_root/surfaceWB
 ##############################################################
 TR=1
 onset=3
 ##############################################################
-case $gs in \
-	'yes' | 'y')
-		dir_work=$dir_root/GLMsingle/glm_${glm1d}/surfaceWB/$subj
-	;;
-	'no' | 'n')
-		dir_work=$dir_root/surfaceWB/glm_${glm1d}/$subj
-	;;
-	*)
-		dir_work=$dir_root/glm_${glm1d}
-	;;
-esac
-
-case $map in \
-	'beta')
-		fname="$dir_work/$subj.$hem.glm_${glm1d}.$map.dtseries.nii"
+case $type_ in \
+	'dseries')
 		wb_command \
-			-cifti-create-dense-timeseries $fname \
-	    	-left-metric $dir_work/$subj.$hem.glm_${glm1d}.$map.func.gii \
+			-cifti-create-dense-timeseries $input \
+	    	-left-metric $dir_surf/$subj/$subj.L.glm_${glm1d}.$map.func.gii \
+	    	-right-metric $dir_surf/$subj/$subj.R.glm_${glm1d}.$map.func.gii \
 		    -timestep $TR -timestart $onset
-		echo " Created $fname"
 	;;	
-	*)
-		fname="$dir_work/$subj.$hem.glm_${glm1d}.$map.dscalar.nii"
+	'dscalar')
 		wb_command \
-			-cifti-create-dense-scalar $fname \
-	    	-left-metric $dir_work/$subj.$hem.glm_${glm1d}.$map.func.gii
-		echo " Created $fname"
+			-cifti-create-dense-scalar $input \
+	    	-left-metric $dir_surf/$subj/$subj.L.glm_${glm1d}.$map.func.gii \
+	    	-right-metric $dir_surf/$subj/$subj.R.glm_${glm1d}.$map.func.gii \
 	;;
 esac
-
+echo " Created $fname"
